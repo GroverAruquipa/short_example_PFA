20cb4eb26a19c71f6acb6fe3744a84a44fff7700
GroverAruquipa <52639144+GroverAruquipa@users.noreply.github.com>
2026-07-01 11:17:28 -0400

    Add files via upload

---
+++ foot_sim-20260701T151204Z-3-001/foot_sim/footbot_diffusion.cpp
@@ -0,0 +1,97 @@
+/* Include the controller definition */
+#include "footbot_diffusion.h"
+/* Function definitions for XML parsing */
+#include <argos3/core/utility/configuration/argos_configuration.h>
+/* 2D vector definition */
+#include <argos3/core/utility/math/vector2.h>
+
+/****************************************/
+/****************************************/
+
+CFootBotDiffusion::CFootBotDiffusion() :
+   m_pcWheels(NULL),
+   m_pcProximity(NULL),
+   m_cAlpha(10.0f),
+   m_fDelta(0.5f),
+   m_fWheelVelocity(2.5f),
+   m_cGoStraightAngleRange(-ToRadians(m_cAlpha),
+                           ToRadians(m_cAlpha)) {}
+
+/****************************************/
+/****************************************/
+
+void CFootBotDiffusion::Init(TConfigurationNode& t_node) {
+   /*
+    * Get sensor/actuator handles
+    *
+    * The passed string (ex. "differential_steering") corresponds to the
+    * XML tag of the device whose handle we want to have. For a list of
+    * allowed values, type at the command prompt:
+    *
+    * $ argos3 -q actuators
+    *
+    * to have a list of all the possible actuators, or
+    *
+    * $ argos3 -q sensors
+    *
+    * to have a list of all the possible sensors.
+    *
+    * NOTE: ARGoS creates and initializes actuators and sensors
+    * internally, on the basis of the lists provided the configuration
+    * file at the <controllers><footbot_diffusion><actuators> and
+    * <controllers><footbot_diffusion><sensors> sections. If you forgot to
+    * list a device in the XML and then you request it here, an error
+    * occurs.
+    */
+   m_pcWheels    = GetActuator<CCI_DifferentialSteeringActuator>("differential_steering");
+   m_pcProximity = GetSensor  <CCI_FootBotProximitySensor      >("footbot_proximity"    );
+   /*
+    * Parse the configuration file
+    *
+    * The user defines this part. Here, the algorithm accepts three
+    * parameters and it's nice to put them in the config file so we don't
+    * have to recompile if we want to try other settings.
+    */
+   GetNodeAttributeOrDefault(t_node, "alpha", m_cAlpha, m_cAlpha);
+   m_cGoStraightAngleRange.Set(-ToRadians(m_cAlpha), ToRadians(m_cAlpha));
+   GetNodeAttributeOrDefault(t_node, "delta", m_fDelta, m_fDelta);
+   GetNodeAttributeOrDefault(t_node, "velocity", m_fWheelVelocity, m_fWheelVelocity);
+}
+
+/****************************************/
+/****************************************/
+
+void CFootBotDiffusion::ControlStep() {
+   /*fFetch sensor readings and initialize thearepulsion vector */
+   const CCI_FootBotProximitySensor::TReadings& tProx = m_pcProximity->GetReadings();
+   CVector2 cRepulsionVec;
+
+   /* Compute vector sum inspired in repulsive forces based on the potential field method */
+   for(size_t i = 0; i < tProx.size(); ++i) {
+      cRepulsionVec += CVector2(tProx[i].Value, tProx[i].Angle + CRadians::PI);
+   }
+
+   /* Define the heading direction by combining forward motion with potential field correction */
+   CVector2 cSteering = CVector2(1.0f, CRadians::ZERO) + (cRepulsionVec * 2.0f);
+   
+   /* Calculate wheel velocities based on the angular steering error */
+   Real fTurn = cSteering.Angle().GetValue();
+   
+   /* Map  error to wheel velocities to maintain continuous motion(simple-elegant method) */
+   m_pcWheels->SetLinearVelocity(m_fWheelVelocity * (1.0f - std::abs(fTurn)), 
+                                 m_fWheelVelocity * (1.0f + fTurn));
+}
+/****************************************/
+/****************************************/
+
+/*
+ * This statement notifies ARGoS of the existence of the controller.
+ * It binds the class passed as first argument to the string passed as
+ * second argument.
+ * The string is then usable in the configuration file to refer to this
+ * controller.
+ * When ARGoS reads that string in the configuration file, it knows which
+ * controller class to instantiate.
+ * See also the configuration files for an example of how this is used.
+ */
+REGISTER_CONTROLLER(CFootBotDiffusion, "footbot_diffusion_controller")
